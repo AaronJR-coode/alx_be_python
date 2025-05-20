bf3a4134ce0dc7bba30444f5f2721ff840757f13
AaronJR-coode <khombeaaronjunior@gmail.com>
2025-05-20 18:00:25 +0200

    Add files via upload

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+monthly_income = float(input("Enter your monthy income "))
+monthly_expenses = float(input("Enter your monthy expenses "))
+
+monthly_savings = monthly_income - monthly_expenses
+
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(f"\nYour monthly savings are: ${monthly_savings:.2f}")
+print(f"Your projected savings after one year with 5% interest are: ${annual_savings:.2f}")
