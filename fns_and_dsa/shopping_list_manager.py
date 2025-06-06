d00ab6e4bcd01c7dc3e06a35df5369019241388a
AaronJR-coode <khombeaaronjunior@gmail.com>
2025-06-06 16:53:00 +0200

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,5 @@
 def display_menu():
-    print("\n--- Shopping List Manager ---")
+    print(f"\n--- Shopping List Manager ---")
     print("1. Add item")
     print("2. Remove item")
     print("3. View list")
