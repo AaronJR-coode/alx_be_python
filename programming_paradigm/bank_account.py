dac11c5dbdb5fb5a532e4d0d844a2d52e33810bb
AaronJR-coode <khombeaaronjunior@gmail.com>
2025-06-13 11:14:13 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self._balance: }")
+        print(f"Current Balance: ${self._balance:.2f}")
