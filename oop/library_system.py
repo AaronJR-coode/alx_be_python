84be3906a8a1e5f442e73f80a45e656646687925
AaronJR-coode <khombeaaronjunior@gmail.com>
2025-06-20 12:46:41 +0200

    Add files via upload

---
+++ oop/library_system.py
@@ -0,0 +1,43 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} {self.author}"
+
+    
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"EBook: {super().__str__()}, File Size: {self.file_size} KB"
+    
+    
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {super().__str__()}, page count: {self.page_count}"
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book: Book):
+        if not isinstance(book, Book):
+            raise TypeError("Only Book (or subclasses) can be added")
+        self.books.append(book)
+        return f"Created: {book}"
+
+
+    def list_books(self):
+        if not self.books:
+            return "Library is empty."
+        for idx, book in enumerate(self.books):
+            print(f"{book}") 
+
